4e76b0b0973dae367c5fcfd9f42e2ad6595f26a8
danielmahler1 <danielmahler34@gmail.com>
2024-04-12 12:20:46 -0400

    Finished main framework

--- project.cpp
+++ project.cpp
@@ -4,7 +4,29 @@
 
 using namespace std;
 
+void FCFS() {
+    cout << "FCFS" << endl;
+}
+
+void SJF() {
+    cout << "SJF" << endl;
+}
+
+void RR() {
+    cout << "RR" << endl;
+}
+
 int main() {
-    cout << "Hello, World!" << endl;
+    cout << "Choose your simulation\n1. FCFS\n2. SJF\n3. RR\nEnter your selection: " << endl;
+    int choice;
+    cin >> choice;
+    if(choice == 1)
+        FCFS();
+    else if(choice == 2)
+        SJF();
+    else if(choice == 3)
+        RR();
+    else
+        cout << "Invalid choice" << endl;
     return 0;
 }
